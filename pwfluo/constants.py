54477dcc5eaa6f273ceafec5e33387ac3b165c9a
jplumail <jeanplumail76@gmail.com>
2024-08-01 14:16:05 +0200

    fix locale.getencoding() not available on python<3.11

--- pwfluo/constants.py
+++ pwfluo/constants.py
@@ -1,7 +1,20 @@
 import locale
+import sys
 
 NO_INDEX = 0  # This is the index value for single images
-if locale.getencoding() == "UTF-8":
+
+if sys.version_info >= (3, 11):
+
+    def getencoding():
+        return locale.getencoding()
+
+else:
+
+    def getencoding():
+        return locale.getpreferredencoding(False)
+
+
+if getencoding() == "UTF-8":
     MICRON_STR = "μm"
 else:
     MICRON_STR = "um"
